8fd104796dce6e38c47ec72d9d3ca2a70a72262c
Kevin Ilham <kevinilham.ka@gmail.com>
2026-05-13 19:56:07 +0700

    Remove duplicate AuthProvider in pages/crud.js to fix logout state bug

--- pages/crud.js
+++ pages/crud.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import { CourseProvider } from '../src/contexts/CourseContext';
-import { AuthProvider } from '../src/contexts/AuthContext';
 import CrudPage from '../src/pages/CrudPage';
 
-function Crud() {
-  return (
-    <AuthProvider>
-      <CourseProvider>
-        <CrudPage />
-      </CourseProvider>
-    </AuthProvider>
-  );
+export default function Crud() {
+  return <CrudPage />;
 }
-
-export default Crud;
